af7ec89a41a37b8639178c5e28ef412f1edcc66d
Sergey Kopeliovich <burunduk30@gmail.com>
2019-05-30 20:08:41 +0300

    update scripts

--- test.sh
+++ test.sh
@@ -1,22 +1,5 @@
 echoerr() { echo "$@" 1>&2; }
 
-function compileJava {
-	echoerr compileJava: \"$1.java to $1.jar\"
-	echo "Main-Class: $1" > manifest.txt
-	javac $1.java || exit 1
-	jar -cvfm $1.jar manifest.txt *.class || exit 1
-	rm *.class manifest.txt
-}
-function compileKt {
-	echoerr compileKotlin: \"$1.kt to $1.jar\"
-	kotlinc $1.kt -include-runtime -d $1.jar || exit 1
-}
-function compileCpp {
-	echoerr compileCpp: \"$1.cpp\"
-	WINFLAGS=-Wl,--stack=512000000 # for Windows OS
-	# optimization level should be setted in $1.cpp
-	g++ -Wall $WINFLAGS -std=c++14 $1.cpp -o $1 || exit 1 
-}
 function runJar {
 	#echoerr runJar: \"$1.jar\"
 	java -Xss512M -jar $1.jar $2 || exit 1
@@ -27,13 +10,7 @@ function testSolution {
 	($1 < $test_file) 2> "${log_name}" || exit 1
 }
 
-# Compile all
-for f in *.cpp  ; do compileCpp  ${f/.*/}; done
-for f in *.kt   ; do compileKt   ${f/.*/}; done
-for f in *.java ; do compileJava ${f/.*/}; done
-#exit
-
-n=100000
+n=3000000
 
 # name=kt_dfs3
 # compileKt $name
--- wipe.sh
+++ wipe.sh
@@ -1,1 +1,1 @@
-rm *.log *.exe *.jar *.in
+rm *.log *.exe *.jar *.in cpp_dfs1 cpp_dfs2
